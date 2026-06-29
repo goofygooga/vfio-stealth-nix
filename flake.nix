c0b6f7a21bc0e12b9c36bcee6b60269acc92229f
Daaboulex <39669593+Daaboulex@users.noreply.github.com>
2026-06-29 14:36:47 +0200

    chore: adopt nix-packaging-standard v2.7.1 (maintenance dedup of flake.lock-broke-build issues)

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     std = {
-      url = "github:Daaboulex/nix-packaging-standard?ref=v2.7.0";
+      url = "github:Daaboulex/nix-packaging-standard?ref=v2.7.1";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.git-hooks.follows = "git-hooks";
     };
